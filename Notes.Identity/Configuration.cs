a7e72fd9ed91a089b02f6ee3f38a3ce8b4f3462c
YehorOstapchuk <62392669+YehorOstapchuk@users.noreply.github.com>
2021-10-29 13:07:34 +0300

    Добавьте файлы проекта.

---
+++ Notes.Identity/Configuration.cs
@@ -0,0 +1,66 @@
+﻿using IdentityModel;
+using IdentityServer4;
+using IdentityServer4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Notes.Identity
+{
+    public class Configuration
+    {
+        public static IEnumerable<ApiScope> ApiScopers =>
+            new List<ApiScope>
+            {
+                new ApiScope("NotesWebAPI","Web API")
+            };
+
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile()
+            };
+        public static IEnumerable<ApiResource> ApiResources =>
+            new List<ApiResource>
+            {
+                new ApiResource("NotesWebAPI", "Web API", new[]
+                { JwtClaimTypes.Name})
+                {
+                    Scopes = {"NotesWebApi"}
+                }
+            };
+        public static IEnumerable<Client> Clients =>
+            new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "notes-web-api",
+                    ClientName = "Notes Web",
+                    AllowedGrantTypes = GrantTypes.Code,
+                    RequireClientSecret = false,
+                    RequirePkce = true,
+                    RedirectUris =
+                    {
+
+                    },
+                    AllowedCorsOrigins =
+                    {
+
+                    },
+                    PostLogoutRedirectUris =
+                    {
+
+                    },
+                    AllowedScopes =
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        "NotesWebAPI"
+                    },
+                    AllowAccessTokensViaBrowser = true
+                }
+            };
+    }
+}
